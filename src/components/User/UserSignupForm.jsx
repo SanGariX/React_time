9ffc7ffa36a40228e82fae4b83487a17483f4cc9
SanGariX <denya.garkusha.12@gmail.com>
2025-01-31 19:09:31 +0200

    corect moment

--- src/components/User/UserSignupForm.jsx
+++ src/components/User/UserSignupForm.jsx
@@ -14,7 +14,7 @@ const UserSignupForm = () => {
 		name: '',
 		email: '',
 		password: '',
-		avatar: 'https://www.google.com/imgres?q=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8&imgurl=https%3A%2F%2Fcn22.nevsedoma.com.ua%2Fp%2F25%2F2583%2F105_files%2F231781_1_nevsedoma_com_ua.jpg&imgrefurl=https%3A%2F%2Fnevsedoma.com.ua%2F557891-kartinki-i-risunki-kotorye-zastavlyayut-zadumatsya-15-foto.html&docid=5MVaSaZvMYQoZM&tbnid=2dXQUPNbYSKa_M&vet=12ahUKEwibi9L5paCLAxX9KRAIHdYpICAQM3oECEkQAA..i&w=640&h=634&hcb=2&ved=2ahUKEwibi9L5paCLAxX9KRAIHdYpICAQM3oECEkQAA',
+		avatar: 'https://img.freepik.com/free-photo/beautiful-selective-focus-shot-crystal-ball-reflecting-breathtaking-sunset_181624-8579.jpg?semt=ais_hybrid',
 	})
 	const handleChange = (evt) => {
 		const target = evt.target
